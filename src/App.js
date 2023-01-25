593a7d44eaf542203333084ade553838c79d8a88
ErJump <fedelijunior@gmail.com>
2023-01-25 10:08:28 +0100

    feat: :sparkles: Adds slider component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container-lg py-5">
+        <h1 className="text-center mb-4">Your magic Holidays</h1>
+        <div className="row">
+          <Slider />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Slider() {
+  return (
+    <div className='col-12'>
+        <h2>Slider</h2>
+    </div>
+  )
+}
