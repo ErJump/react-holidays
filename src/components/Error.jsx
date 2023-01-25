c5181ea2f6b7b79330152c1a16c5ea146803d67d
ErJump <fedelijunior@gmail.com>
2023-01-25 11:50:43 +0100

    feat: :sparkles: Adds error and loader component

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div className='col-12'>
+        <h2 className='text-center'>Something went wrong...</h2>            
+    </div>
+  )
+}
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <div className='col-12'>
+        <h2 className='text-center'>Loading...</h2>
+    </div>
+  )
+}
