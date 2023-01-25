3af2eef38d3bcecd4a2e46fe8fbd4691395a1667
ErJump <fedelijunior@gmail.com>
2023-01-25 11:33:51 +0100

    feat: :sparkles: Adds goNext and goPrev methods to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Slider from "./components/Slider";
 import {useState, useEffect} from 'react';
 import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 
 const url = "https://react--course-api.herokuapp.com/api/v1/data/vacanze";
 
@@ -25,7 +27,19 @@ function App() {
   }
 
   const goNext = () => {
+    if(activeIndex < holidays.length - 1 )
+      setActiveIndex((oldValue) => {
+        return oldValue + 1
+      })
+    else setActiveIndex(0)
+  }
 
+  const goPrev = () => {
+    if (activeIndex > 0)
+      setActiveIndex((oldValue) => {
+        return oldValue - 1
+      })
+    else setActiveIndex(holidays.length -1)
   }
 
   useEffect(() => {
@@ -36,8 +50,10 @@ function App() {
     <div className="App">
       <div className="container-lg py-5">
         <h1 className="text-center mb-4">Your magic Holidays</h1>
-        <div className="row justify-content-center">
+        <div className="row justify-content-center align-items-center">
+          <button type="button" className="col-1 btn btn-primary" onClick={goPrev}><FontAwesomeIcon icon={faAngleLeft}/></button>
           <Slider activeIndex={activeIndex} holidays={holidays} isError={isError} isLoading={isLoading}/>
+          <button type="button" className="col-1 btn btn-primary" onClick={goNext}><FontAwesomeIcon icon={faAngleRight}/></button>
         </div>
       </div>
     </div>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -15,7 +15,7 @@ export default function Slider(props) {
     )
 
     return (
-        <div className='col-8 card px-0'>
+        <div className='col-8 card px-0 mx-3'>
             <img src={props.holidays[props.activeIndex].img} className="card-img-top" alt="holiday img"/>
             <div className="card-body">
                 <h5 className="card-title">{props.holidays[props.activeIndex].titolo}</h5>
